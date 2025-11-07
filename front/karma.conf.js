4a06d2f6e7fc23d78d259fd22fe5687127cd7d59
BuildTools <unconfigured@null.spigotmc.org>
2025-11-07 15:22:28 +0100

    karma install junit

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -32,7 +32,12 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml','junit'],
+	junitReporter: {
+      outputDir: 'test-results', // dossier relatif au working-directory (front/)
+      outputFile: 'unit-test-results.xml',
+      useBrowserName: false
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
