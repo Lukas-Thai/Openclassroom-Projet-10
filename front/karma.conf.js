ee36247248d03518687f930611d20e2847165196
BuildTools <unconfigured@null.spigotmc.org>
2025-11-07 15:27:47 +0100

    add missing import

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -32,18 +32,13 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml','junit'],
-	junitReporter: {
-      outputDir: 'test-results', // dossier relatif au working-directory (front/)
-      outputFile: 'unit-test-results.xml',
-      useBrowserName: false
-    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
     restartOnFileChange: true
   });
 };
